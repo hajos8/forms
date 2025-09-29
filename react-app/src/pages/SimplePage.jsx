1249e1d468e1d93468f9a96f46b517a2664bd085
hajos8 <rabb.andras@gmail.com>
2025-09-29 08:35:18 +0200

    handleFormSubmit

--- react-app/src/pages/SimplePage.jsx
+++ react-app/src/pages/SimplePage.jsx
@@ -1,14 +1,24 @@
 /* SimplePage.jsx */
 import React from "react";
 export default class SimplePage extends React.Component {
+    state = {
+        
+    }
+
+    handleFormSubmit = e =>{
+        e.preventDefault()
+        console.log(e)
+    }
+
     render() {
         return (
             <div id="content-simple" className="tab-content active">
             <h2>Simple Email Form</h2>
-                <form>
+                <form onSubmit={this.handleFormSubmit}>
                     <label htmlFor="simpleEmail">Email address:</label>
                     <input type="email" id="simpleEmail" name="simpleEmail" placeholder="name@example.com" required autoComplete="on" />
                     <button type="submit">Submit</button>
+                    <input type="reset" value={"Reset"}/>
                 </form>
             </div>
         )
